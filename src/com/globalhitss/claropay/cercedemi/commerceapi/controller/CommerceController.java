3e5b0102ab65d3a0f201c72e193ac4a094b27fc2
Ricardo Bermúdez Bermúdez <ricardob.sistemas@gmail.com>
2019-11-06 20:12:10 -0600

    add controllers

---
+++ src/com/globalhitss/claropay/cercedemi/commerceapi/controller/CommerceController.java
@@ -0,0 +1,21 @@
+package com.globalhitss.claropay.cercedemi.commerceapi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/commerce")
+public class CommerceController {
+
+	public void getCommerceByGeolocation()
+	{
+		
+	}
+	
+	public void getCommerceByID()
+	{
+		
+	}
+}
---
+++ src/com/globalhitss/claropay/cercedemi/commerceapi/controller/IPLocationController.java
@@ -0,0 +1,16 @@
+package com.globalhitss.claropay.cercedemi.commerceapi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/ip-location")
+public class IPLocationController {
+    
+	public void getGeoLocationByIP()
+	{
+		
+	}
+}
